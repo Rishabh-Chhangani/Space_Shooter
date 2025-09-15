3ccdeef36c31de338582ab3d8553215b33146433
Rishabh <rishabh.chhangani@gmail.com>
2025-09-15 21:11:44 +0530

    Updated : A liitle bit

--- code/main.py
+++ code/main.py
@@ -7,8 +7,9 @@ from random import randint
 pygame.init()
 Window_Width , Window_Height = 1280 , 660
 display_surface = pygame.display.set_mode((Window_Width , Window_Height))
-running = True 
 pygame.display.set_caption('SPACE SHOOTER')
+running = True 
+clock = pygame.time.Clock()
 
 # Plain Surface 
 surf = pygame.Surface((100,200))
@@ -18,7 +19,8 @@ x = 100
 # Importing an Image
 player_surf = pygame.image.load(join('images','player.png')).convert_alpha()
 player_rect = player_surf.get_frect(center=(Window_Width / 2,Window_Height / 2))
-player_direction = 1
+player_direction = pygame.Vector2(1,1)
+player_speed = 300
 
 star_surf = pygame.image.load(join('images','star.png')).convert_alpha()
 star_pos = [(randint(0,Window_Width),randint(0,Window_Height)) for i in range(20)]
@@ -32,6 +34,8 @@ laser_rect = laser_surf.get_frect(center =( 20 , Window_Height - 40 ))
 
 # Game Loop
 while running:
+    dt = clock.tick() / 1000 #dt is dealt ta time 
+
     # event Loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -45,12 +49,13 @@ while running:
     display_surface.blit(meteor_surf,meteor_rect)
     display_surface.blit(laser_surf , laser_rect)
 
+    if player_rect.bottom >= Window_Height or player_rect.top <= 0:
+        player_direction.y *= -1
+    if player_rect.right >= Window_Width or player_rect.left <= 0:
+        player_direction.x *= -1
 
-    player_rect.x += player_direction * 0.3
-    if player_rect.right > Window_Width or player_rect.left < 0:
-        player_direction *= -1
-    # if player_rect.right < Window_Width:
-    #     player_rect.left += 0.2
+
+    player_rect.center += player_direction * player_speed * dt
     display_surface.blit(player_surf,player_rect)
    
 
